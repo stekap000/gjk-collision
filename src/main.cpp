9bac4043144fb02525488875b6cc407a4fce0c1a
stekap000 <stekap000@gmail.com>
2024-06-11 01:31:52 +0200

    1-simplex

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-// TODO: Implement original GJK
 // TODO: Implement optimizations (Erickson, Muratori, ...) [A Geometric Interpretation of the Boolean GJK Algorithm] https://arxiv.org/pdf/1505.07873
 // TODO: 2D case, 3D case
 
@@ -38,51 +37,54 @@ v3 minkowski_diff_support(shape_2d s1, shape_2d s2, v3 direction) {
 	return support(s1, direction) - support(s2, -direction);
 }
 
-v3 closest_simplex_point(simplex s) {
-	switch(s.n) {
-	case 1: {
-		return s.points[0];
-	}
+void add_point_to_simplex(simplex *s, v3 point) {
+	s->points[n++] = point;
+}
+
+bool update_simplex_and_direction(simplex *s, v3 *d) {
+	// One point case doesn't happen.
+
+	// Two point case is determined because of how we found support point and
+	// because of check if support point didn't reach origin (in the main loop).
+	switch(s->n) {
 	case 2: {
-		
+		v3 temp = s->points[0] - s->points[1];
+		*d = v3::cross(v3::cross(temp, -(s->points[1])), temp);
 	}
 	case 3: {
-
+		
 	}
 	case 4: {
 
 	}
 	}
+
+	return false;
 }
 
-v3 gjk(shape_2d s1, shape_2d s2) {
+bool gjk(shape_2d s1, shape_2d s2) {
 	simplex s;
+	v3 direction;
 	// Start out with some point in minkowski diff.
 	v3 maybe_closest = pick_from_md(s1, s2);
-	add_point_to_simplex(s, maybe_closest);
+	add_point_to_simplex(&s, maybe_closest);
+
+	direction = -maybe_closest;
 
 	while(true) {
 		// Furthest point in opposite direction (direction towards origin).
-		v3 support_point = minkowski_diff_support(s1, s2, -maybe_closest);
+		v3 support_point = minkowski_diff_support(s1, s2, direction);
 
-		// We are moving towards origin and there is no point that is closer to origin than our starting point.
-		// This means that starting point is on the edge of minkowski diff and is closest to origin, so we return it.
-		if(support_point == maybe_closest /* up to error */) {
-			return support_point;
-		}
-	
-		add_point_to_simplex(s, support_point);
-		maybe_closest = closest_simplex_point(s);
-		// Only preserve points of sub-simplex that contains simplex point closest to origin.
-		// We will build new simplex from piece of the old one that is closest to origin.
-		cull_simplex(s);
+		// Support point didn't reach origin.
+		if(v3::dot(support_point, direction) < 0) return false;
+
+		add_point_to_simplex(&s, support_point);
+
+		if (update_simplex_and_direction(&s, &direction)) return true;
 	}
 }
 
-
-
 int main() {
-	//std::cout << "Works!" << std::endl;
 	
 	return 0;
 }
